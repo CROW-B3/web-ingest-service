a74a50887a121f557b0abf0c6f132c4ccf5b7395
Methika Fernando <methikafernando2006@gmail.com>
2025-12-27 02:23:08 +0530

    Revert "feat: integrate Drizzle Studio for database management"
    This reverts commit b737043c8140d484886319b9ff31d4fbd17ebd6e.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,3 @@
-import process from 'node:process';
-
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
@@ -7,11 +5,4 @@ export default defineConfig({
   schema: './src/db/schema.ts',
   dialect: 'sqlite',
   driver: 'd1-http',
-  dbCredentials: {
-    accountId:
-      process.env.CLOUDFLARE_ACCOUNT_ID || '8f0203259905d8923687286c84921e6c',
-    databaseId:
-      process.env.D1_DATABASE_ID || 'e73411e4-8c1b-48a6-ad20-2bd74249d29a',
-    token: process.env.CLOUDFLARE_API_TOKEN || '',
-  },
 });
